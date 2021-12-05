405b2e4adc0a2553d9b9053a78f9094135c51b6a
Adeptus Noobus <adeptus.noobus@gmail.com>
2021-12-05 18:04:14 +0000

    Fix hero portrait URLs

--- components/hero.tsx
+++ components/hero.tsx
@@ -20,48 +20,48 @@ export default Hero;
 function getHeroPortrait(hero: number) {
 	switch (hero) {
 		case 1:
-			return '/assets/img/dow2/portraits/sm_force_commander.png';
+			return '/img/portraits/sm_force_commander.png';
 		case 2:
-			return '/assets/img/dow2/portraits/sm_apothecary.png';
+			return '/img/portraits/sm_apothecary.png';
 		case 3:
-			return '/assets/img/dow2/portraits/sm_techmarine.png';
+			return '/img/portraits/sm_techmarine.png';
 		case 4:
-			return '/assets/img/dow2/portraits/ork_warboss.png';
+			return '/img/portraits/ork_warboss.png';
 		case 5:
-			return '/assets/img/dow2/portraits/ork_kommando_hero.png';
+			return '/img/portraits/ork_kommando_hero.png';
 		case 6:
-			return '/assets/img/dow2/portraits/ork_mek_boy.png';
+			return '/img/portraits/ork_mek_boy.png';
 		case 7:
-			return '/assets/img/dow2/portraits/eld_warlock_hero.png';
+			return '/img/portraits/eld_warlock.png';
 		case 8:
-			return '/assets/img/dow2/portraits/eld_warp_spider_hero.png';
+			return '/img/portraits/eld_warp_spider_hero.png';
 		case 9:
-			return '/assets/img/dow2/portraits/eld_farseer.png';
+			return '/img/portraits/eld_farseer.png';
 		case 10:
-			return '/assets/img/dow2/portraits/tyr_hive_tyrant.png';
+			return '/img/portraits/tyr_hive_tyrant.png';
 		case 11:
-			return '/assets/img/dow2/portraits/tyr_ravener_hero.png';
+			return '/img/portraits/tyr_ravener_hero.png';
 		case 12:
-			return '/assets/img/dow2/portraits/tyr_lictor_hero.png';
+			return '/img/portraits/tyr_lictor_hero.png';
 		case 13:
-			return '/assets/img/dow2/portraits/csm_chaos_lord.png';
+			return '/img/portraits/csm_chaos_lord.png';
 		case 14:
-			return '/assets/img/dow2/portraits/csm_plague_marine_champion.png';
+			return '/img/portraits/csm_plague_marine_champion.png';
 		case 15:
-			return '/assets/img/dow2/portraits/csm_sorcerer.png';
+			return '/img/portraits/csm_sorcerer.png';
 		case 16:
-			return '/assets/img/dow2/portraits/ig_inquisitor.png';
+			return '/img/portraits/ig_inquisitor.png';
 		case 17:
-			return '/assets/img/dow2/portraits/ig_commissar_lord.png';
+			return '/img/portraits/ig_commissar_lord.png';
 		case 18:
-			return '/assets/img/dow2/portraits/ig_commander.png';
+			return '/img/portraits/ig_commander.png';
 		case 19:
-			return '/assets/img/dow2/portraits/gk_brother_captain.png';
+			return '/img/portraits/gk_brother_captain.png';
 		case 20:
-			return '/assets/img/dow2/portraits/gk_eversor_hero.png';
+			return '/img/portraits/gk_eversor_hero.png';
 		case 21:
-			return '/assets/img/dow2/portraits/gk_daemonhunter_hero.png';
+			return '/img/portraits/gk_daemonhunter_hero.png';
 		default:
-			return '/assets/img/dow2/portraits/gk_daemonhunter_hero.png';
+			return '/img/portraits/gk_daemonhunter_hero.png';
 	}
 }
