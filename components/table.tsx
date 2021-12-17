2861db2c5e2dc2640d6727650b867cd92e833ff5
Adeptus Noobus <adeptus.noobus@gmail.com>
2021-12-17 23:18:03 +0000

    Fix table styles

--- components/table.tsx
+++ components/table.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react';
 export function DowTable(props: PropsWithChildren<any>) {
 	const { headers, totalElements }: { headers: string[]; totalElements: number } = props;
 	return (
-		<div className="py-2 align-middle inline-block w-8/12 sm:px-6 lg:px-8">
+		<div className="py-2 mx-auto w-8/12 sm:px-6 lg:px-8">
 			<div className="drop-shadow-xl sm:rounded-lg">
 				<table className="table w-full">
 					<thead>
