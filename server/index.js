35b92600e9cdadd21dddc5f15f253fa58e5efedd
Adeptus Noobus <adeptus.noobus@gmail.com>
2021-12-18 00:20:15 +0000

    wip add redis cache

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const app = express();
 const matches = require('./routes/matches')
 const ladder = require('./routes/ladder')
 const players = require('./routes/players')
+const statistics = require('./routes/statistics')
 
 app.use(morgan('dev'))
 
 app.use('/matches', matches)
 app.use('/ladder', ladder)
 app.use('/players', players)
+app.use('/statistics', statistics)
 
 app.listen(4000, () => {
   console.log('Listening on port:', 4000)
