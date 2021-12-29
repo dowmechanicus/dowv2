bf26d5ecb3a8e30382fd85970686f704588231b6
Adeptus Noobus <adeptus.noobus@gmail.com>
2021-12-29 16:06:50 +0000

    Restrict available themes to dark and light

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  mode: 'jit',
   purge: ['./pages/**/*.{js, ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
+  themes: [
+    'light',
+    'dark'
+  ],
   theme: {
     extend: {
       maxHeight: {
