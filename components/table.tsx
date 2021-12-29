165eda9d84f25adbbc2ea73dd5ccbec379789eef
Adeptus Noobus <adeptus.noobus@gmail.com>
2021-12-29 16:13:54 +0000

    Remove tabs in favor of whitespaces

--- components/table.tsx
+++ components/table.tsx
@@ -2,30 +2,30 @@ import Pagination from '@/components/pagination';
 import { PropsWithChildren } from 'react';
 
 export function DowTable(props: PropsWithChildren<any>) {
-	const { headers, totalElements }: { headers: string[]; totalElements: number } = props;
-	return (
-		<div className="py-2 mx-auto w-8/12 sm:px-6 lg:px-8">
-			<div className="drop-shadow-xl sm:rounded-lg">
-				<table className="table w-full">
-					<thead>
-						<tr>
-							{headers.map((header, index) => (
-								<th key={`${header}_${index}`} scope="col" className="uppercase tracking-wider">
-									{header}
-								</th>
-							))}
-						</tr>
-					</thead>
-					<tbody className="">{props.children}</tbody>
-				</table>
-			</div>
-			{totalElements ? <Pagination totalElements={totalElements} /> : null}
-		</div>
-	);
+  const { headers, totalElements }: { headers: string[]; totalElements: number } = props;
+  return (
+    <div className="py-2 mx-auto w-8/12 sm:px-6 lg:px-8">
+      <div className="drop-shadow-xl sm:rounded-lg">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              {headers.map((header, index) => (
+                <th key={`${header}_${index}`} scope="col" className="uppercase tracking-wider">
+                  {header}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="">{props.children}</tbody>
+        </table>
+      </div>
+      {totalElements ? <Pagination totalElements={totalElements} /> : null}
+    </div>
+  );
 }
 
 export function DefaultField(props: PropsWithChildren<any>) {
-	const { className } = props;
+  const { className } = props;
 
-	return <td className={`${className} whitespace-nowrap`}>{props.children}</td>;
+  return <td className={`${className} whitespace-nowrap`}>{props.children}</td>;
 }
--- pages/matches.tsx
+++ pages/matches.tsx
@@ -6,150 +6,150 @@ import { PageResponse } from '@/lib/page-response.interface';
 import { useRouter } from 'next/router';
 
 const Matches = ({ data: matches, totalElements }: PageResponse<Match>) => {
-	const headers: string[] = ['Date', 'Player 1', 'Player 2', 'Map'];
-	const router = useRouter();
+  const headers: string[] = ['Date', 'Player 1', 'Player 2', 'Map'];
+  const router = useRouter();
 
-	const show_match = (match_id: number) => {
-		router.push(`${router.asPath}/${match_id}`);
-	};
+  const show_match = (match_id: number) => {
+    router.push(`${router.asPath}/${match_id}`);
+  };
 
-	return (
-		<DowTable headers={headers} totalElements={totalElements}>
-			{matches ? (
-				matches.map((match: Match) => (
-					<tr
-						key={match.id}
-						className="hover:bg-gray-300 transition-colors hover:cursor-pointer"
-						onClick={() => show_match(match.id)}
-					>
-						<MatchRow match={match} />
-					</tr>
-				))
-			) : (
-				<tr>
-					<td colSpan={4} className="text-center">
-						No data available
-					</td>
-				</tr>
-			)}
-		</DowTable>
-	);
+  return (
+    <DowTable headers={headers} totalElements={totalElements}>
+      {matches ? (
+        matches.map((match: Match) => (
+          <tr
+            key={match.id}
+            className="hover:bg-gray-300 transition-colors hover:cursor-pointer"
+            onClick={() => show_match(match.id)}
+          >
+            <MatchRow match={match} />
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={4} className="text-center">
+            No data available
+          </td>
+        </tr>
+      )}
+    </DowTable>
+  );
 };
 
 export async function getServerSideProps({ query: { page = 1 } }) {
-	const res = await fetch(`${process.env.API_URL}/matches?offset=${page}`);
-	const { data = [], totalElements = 0 }: PageResponse<Match> = await res.json();
+  const res = await fetch(`${process.env.API_URL}/matches?offset=${page}`);
+  const { data = [], totalElements = 0 }: PageResponse<Match> = await res.json();
 
-	return {
-		props: {
-			data,
-			totalElements,
-		},
-	};
+  return {
+    props: {
+      data,
+      totalElements,
+    },
+  };
 }
 
 export default Matches;
 
 const MatchRow = ({ match }: { match: Match }) => {
-	return (
-		<>
-			<DefaultField>
-				<span className="text-xs text-gray-400">{new Date(match.unix_utc_time * 1000).toLocaleString()}</span>
-			</DefaultField>
-			<DefaultField>
-				<PlayerField
-					name={match.p1_name}
-					hero={match.p1_hero}
-					rating={match.p1_rating}
-					rd={match.p1_rd}
-					outcome_rating={match.p1_outcome_rating}
-					outcome_rd={match.p1_outcome_rd}
-					winner={match.winner === 1}
-				/>
-			</DefaultField>
-			<DefaultField>
-				<PlayerField
-					name={match.p2_name}
-					hero={match.p2_hero}
-					rating={match.p2_rating}
-					rd={match.p2_rd}
-					outcome_rating={match.p2_outcome_rating}
-					outcome_rd={match.p2_outcome_rd}
-					winner={match.winner === 2}
-				/>
-			</DefaultField>
-			<DefaultField>
-				<div className="flex flex-col">
-					<span>{match.map_name}</span>
-					<span className="text-xs text-gray-400">{ticks2time(match.ticks)}</span>
-				</div>
-			</DefaultField>
-		</>
-	);
+  return (
+    <>
+      <DefaultField>
+        <span className="text-xs text-gray-400">{new Date(match.unix_utc_time * 1000).toLocaleString()}</span>
+      </DefaultField>
+      <DefaultField>
+        <PlayerField
+          name={match.p1_name}
+          hero={match.p1_hero}
+          rating={match.p1_rating}
+          rd={match.p1_rd}
+          outcome_rating={match.p1_outcome_rating}
+          outcome_rd={match.p1_outcome_rd}
+          winner={match.winner === 1}
+        />
+      </DefaultField>
+      <DefaultField>
+        <PlayerField
+          name={match.p2_name}
+          hero={match.p2_hero}
+          rating={match.p2_rating}
+          rd={match.p2_rd}
+          outcome_rating={match.p2_outcome_rating}
+          outcome_rd={match.p2_outcome_rd}
+          winner={match.winner === 2}
+        />
+      </DefaultField>
+      <DefaultField>
+        <div className="flex flex-col">
+          <span>{match.map_name}</span>
+          <span className="text-xs text-gray-400">{ticks2time(match.ticks)}</span>
+        </div>
+      </DefaultField>
+    </>
+  );
 };
 
 const PlayerField = ({
-	hero,
-	name,
-	rating,
-	rd,
-	outcome_rating,
-	outcome_rd,
-	winner,
+  hero,
+  name,
+  rating,
+  rd,
+  outcome_rating,
+  outcome_rd,
+  winner,
 }: {
-	name: string;
-	hero: number;
-	rating: number;
-	rd: number;
-	outcome_rating: number;
-	outcome_rd: number;
-	winner: boolean;
+  name: string;
+  hero: number;
+  rating: number;
+  rd: number;
+  outcome_rating: number;
+  outcome_rd: number;
+  winner: boolean;
 }) => {
-	return (
-		<div className="flex flex-row items-left gap-8">
-			<div className="flex flex-col items-left">
-				<Hero size={50} hero={hero} />
-			</div>
-			<div className="flex flex-col items-left">
-				<span>{name}</span>
-				<Ratings rating={{ glicko_rating: rating, rd: rd }} />
-				<Ratings rating={{ glicko_rating: outcome_rating, rd: outcome_rd }} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-row items-left gap-8">
+      <div className="flex flex-col items-left">
+        <Hero size={50} hero={hero} />
+      </div>
+      <div className="flex flex-col items-left">
+        <span>{name}</span>
+        <Ratings rating={{ glicko_rating: rating, rd: rd }} />
+        <Ratings rating={{ glicko_rating: outcome_rating, rd: outcome_rd }} />
+      </div>
+    </div>
+  );
 };
 
 export interface Match {
-	id: number;
-	match_relic_id: number;
-	automatch: boolean;
-	md5: string;
-	p1_relic_id: number;
-	p2_relic_id: number;
-	p1_name: string;
-	p2_name: string;
-	p1_hero: number;
-	p2_hero: number;
-	p1_rank: number;
-	p2_rank: number;
-	p1_rating: number;
-	p2_rating: number;
-	p1_rd: number;
-	p2_rd: number;
-	p1_outcome_rating: number;
-	p2_outcome_rating: number;
-	p1_outcome_rd: number;
-	p2_outcome_rd: number;
-	map: number;
-	ticks: number;
-	winner: 1 | 2;
-	ranked: number;
-	unix_utc_time: number;
-	mod_version: number;
-	chat: string;
-	observers: string;
-	youtube: string;
-	league: 0 | 1;
-	map_name: string;
-	file_name: string;
+  id: number;
+  match_relic_id: number;
+  automatch: boolean;
+  md5: string;
+  p1_relic_id: number;
+  p2_relic_id: number;
+  p1_name: string;
+  p2_name: string;
+  p1_hero: number;
+  p2_hero: number;
+  p1_rank: number;
+  p2_rank: number;
+  p1_rating: number;
+  p2_rating: number;
+  p1_rd: number;
+  p2_rd: number;
+  p1_outcome_rating: number;
+  p2_outcome_rating: number;
+  p1_outcome_rd: number;
+  p2_outcome_rd: number;
+  map: number;
+  ticks: number;
+  winner: 1 | 2;
+  ranked: number;
+  unix_utc_time: number;
+  mod_version: number;
+  chat: string;
+  observers: string;
+  youtube: string;
+  league: 0 | 1;
+  map_name: string;
+  file_name: string;
 }
