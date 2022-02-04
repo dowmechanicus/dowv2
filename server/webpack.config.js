08d96bc4a5b1c9b260fc81f9d6ac5e3dc425fffd
Adeptus Noobus <adeptus.noobus@gmail.com>
2022-02-04 23:36:09 +0100

    Add webpack to dependencies so Express server can be bundled with it

---
+++ server/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    'index': './index.js',
+  },
+  target: 'node',
+  resolve: {
+    extensions: ['.js']
+  },
+  externals: [nodeExternals()],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '.js'),
+  }
+};
