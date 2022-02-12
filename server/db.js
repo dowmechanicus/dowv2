8384a1fe9148a8711c85a5e4a377416e24037e0a
Adeptus Noobus <adeptus.noobus@gmail.com>
2022-02-12 16:50:26 +0100

    Make MYSQL configuration via environment variables possible

--- server/db.js
+++ server/db.js
@@ -2,16 +2,23 @@ const mysql = require('mysql')
 const logger = require('./logger')
 
 const pool = mysql.createPool({
-  database: 'esl',
-  host: 'localhost',
-  port: 3306,
-  user: 'esl',
-  password: 'esl',
+  database: process.env.MYSQL_DB ?? 'esl',
+  host: process.env.MYSQL_HOST ?? 'localhost',
+  port: process.env.MYSQL_PORT ?? 3306,
+  user: process.env.MYSQL_USER ?? 'esl',
+  password: process.env.MYSQL_PASSWORD ?? 'esl',
   bigNumberStrings: true,
   supportBigNumbers: true
 });
 
-pool.query('SELECT 1;', (error, results) => error ? process.exit(1) : logger.info('Successfully connected to database'));
+pool.query('SELECT 1;', (error, results) => {
+  if (error) {
+    logger.error(`Could not connect to database. ${error}\n`);
+    process.exit(1);
+  }
+
+  logger.info('Successfully connected to database')
+});
 
 function query(query, params) {
   logger.debug(`SQL Query: ${query}\nSQL Query Parameters: ${params}`);
