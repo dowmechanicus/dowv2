ebe008e7dae06c8e5dda3d0f4eef4845d1d9e6b8
Adeptus Noobus <adeptus.noobus@gmail.com>
2022-10-30 18:00:00 +0100

    Remove /v2 prefix from base url as it will no longer be used once app is released

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div className='flex flex-col'>
-      <Router basename='/v2'>
+      <Router>
         <Navbar />
         <AppRoutes />
       </Router>
--- frontend/src/components/hero.js
+++ frontend/src/components/hero.js
@@ -17,48 +17,48 @@ export default Hero;
 function getHeroPortrait(hero) {
   switch (hero) {
     case 1:
-      return '/v2/img/portraits/sm_force_commander.png';
+      return '/img/portraits/sm_force_commander.png';
     case 2:
-      return '/v2/img/portraits/sm_apothecary.png';
+      return '/img/portraits/sm_apothecary.png';
     case 3:
-      return '/v2/img/portraits/sm_techmarine.png';
+      return '/img/portraits/sm_techmarine.png';
     case 4:
-      return '/v2/img/portraits/ork_warboss.png';
+      return '/img/portraits/ork_warboss.png';
     case 5:
-      return '/v2/img/portraits/ork_kommando_hero.png';
+      return '/img/portraits/ork_kommando_hero.png';
     case 6:
-      return '/v2/img/portraits/ork_mek_boy.png';
+      return '/img/portraits/ork_mek_boy.png';
     case 7:
-      return '/v2/img/portraits/eld_warlock.png';
+      return '/img/portraits/eld_warlock.png';
     case 8:
-      return '/v2/img/portraits/eld_warp_spider_hero.png';
+      return '/img/portraits/eld_warp_spider_hero.png';
     case 9:
-      return '/v2/img/portraits/eld_farseer.png';
+      return '/img/portraits/eld_farseer.png';
     case 10:
-      return '/v2/img/portraits/tyr_hive_tyrant.png';
+      return '/img/portraits/tyr_hive_tyrant.png';
     case 11:
-      return '/v2/img/portraits/tyr_ravener_hero.png';
+      return '/img/portraits/tyr_ravener_hero.png';
     case 12:
-      return '/v2/img/portraits/tyr_lictor_hero.png';
+      return '/img/portraits/tyr_lictor_hero.png';
     case 13:
-      return '/v2/img/portraits/csm_chaos_lord.png';
+      return '/img/portraits/csm_chaos_lord.png';
     case 14:
-      return '/v2/img/portraits/csm_plague_marine_champion.png';
+      return '/img/portraits/csm_plague_marine_champion.png';
     case 15:
-      return '/v2/img/portraits/csm_sorcerer.png';
+      return '/img/portraits/csm_sorcerer.png';
     case 16:
-      return '/v2/img/portraits/ig_inquisitor.png';
+      return '/img/portraits/ig_inquisitor.png';
     case 17:
-      return '/v2/img/portraits/ig_commissar_lord.png';
+      return '/img/portraits/ig_commissar_lord.png';
     case 18:
-      return '/v2/img/portraits/ig_commander.png';
+      return '/img/portraits/ig_commander.png';
     case 19:
-      return '/v2/img/portraits/gk_brother_captain.png';
+      return '/img/portraits/gk_brother_captain.png';
     case 20:
-      return '/v2/img/portraits/gk_eversor_hero.png';
+      return '/img/portraits/gk_eversor_hero.png';
     case 21:
-      return '/v2/img/portraits/gk_daemonhunter_hero.png';
+      return '/img/portraits/gk_daemonhunter_hero.png';
     default:
-      return '/v2/img/portraits/gk_daemonhunter_hero.png';
+      return '/img/portraits/gk_daemonhunter_hero.png';
   }
 }
